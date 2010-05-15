c2ef107cc45bbd5788f30b87e097156f1d3e07a4
croland <cbroland@gmail.com>
2010-05-15 16:59:10 -0400

    Updating environment.rg file

--- config/environment.rb
+++ config/environment.rb
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "rspec", :lib => false, :version => ">= 1.2.0"
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
   config.gem 'haml'
-  config.gem 'authlogic'
+  config.gem "authlogic", :version => ">= 2.1.3"
   config.gem "compass", :version => ">= 0.10.1"
   
 
