2423a7f7e49e4c6c21189d3667c7a2461cc8d93d
croland <cbroland@gmail.com>
2010-05-22 16:43:11 -0400

    Working on feature file for adding comments

---
+++ features/add_comments.feature
@@ -0,0 +1,17 @@
+Feature: Build a way for users to comment on a status 
+	In order for a user to encourage another user
+	A user
+	Should add a comment to their status
+
+Scenario: A user can add a comment
+	Given I am logged in as "testy@mctester.com"
+	When I click on a "Comment" link
+	Then a Comment Box appears 
+		And the Comment Box has focus 
+	When the Comment Box blurs without entering text
+	Then I do not see a Comment Box
+	
+	Given I type "Well Done" in a Comment Box
+	When I click Submit
+	Then I should see "Well Done" under the Action
+	And I should not see a Comment Box
---
+++ features/step_definitions/common_steps.rb
@@ -0,0 +1,20 @@
+# Common step definitions
+
+def create_my_user(params)
+	unless user = User.find_by_email(params[:email])
+		params[:password_confirmation] = params[:password]
+		user = User.create(params)
+		user.update_attribute(:confirmation_token, nil)
+		user.update_attribute(:confirmed_at, Time.now)
+	end
+	user
+end
+
+Given /^I am logged in as (.*)$/ do |email|
+	@password = "test123"
+	@current_user = create_my_user(:email => email, :password => @password)
+	visit new_user_session_path
+	fill_in("Email", :with => email)
+	fill_in("Password", :with => @password)
+	click_button("Sign in")
+end
