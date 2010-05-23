0e8e915d93faab4b30aa895da105adad1daee9b9
Len Smith <ignu.smith@gmail.com>
2010-05-23 14:30:12 -0400

    renamed routes to users/login users/logout and users/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
-  map.devise_for :users
+  map.devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => "new"}
   map.resource :users
   map.profile 'users/new', :controller=>"users", :action=>"new"      
   map.profile 'users/create', :controller=>"users", :action=>"create"        
   map.profile 'users/:id', :controller=>"users", :action=>"show"    
-  map.root :controller=>"wins", :action=>"index" 
-  map.resource :wins
+  map.root :controller=>"actions", :action=>"index" 
+  map.resource :actions
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
