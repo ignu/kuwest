6f3769ea62474597f064f69050dbba504c5fb5dc
Len Smith <ignu.smith@gmail.com>
2010-05-29 14:40:57 -0400

    updating with monkey patch info

--- config/initializers/monkey_patchin.rb
+++ config/initializers/monkey_patchin.rb
@@ -1,6 +1,6 @@
 class Float
   def pretty
-    #return ("%.1f" % self).gsub(/(\d)(?=\d{3}+(\.\d*)?$)/, '\1,') if modulo(1) > 0
+    return ("%.1f" % self).gsub(/(\d)(?=\d{3}+(\.\d*)?$)/, '\1,') if modulo(1) > 0
     self.to_i.to_s
   end
 end
