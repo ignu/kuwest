b87d3c49d7180185fc93b893f97e40649ca59258
Matt <matt@matt-ubuntu.(none)>
2010-05-29 17:10:44 -0400

    why did schema change?

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100529075149) do
+ActiveRecord::Schema.define(:version => 20100529191233) do
 
   create_table "comments", :force => true do |t|
     t.string   "body"
