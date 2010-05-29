3da9ebe1e51a78cee6edd2c301d341607549e116
Len Smith <ignu.smith@gmail.com>
2010-05-29 14:19:26 -0400

    ugh float work on heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
-
-end
-class Float
-  def pretty
-    return ("%.1f" % self).gsub(/(\d)(?=\d{3}+(\.\d*)?$)/, '\1,') if modulo(1) > 0
-    self.to_i.to_s
+  class Float
+    def pretty
+      return ("%.1f" % self).gsub(/(\d)(?=\d{3}+(\.\d*)?$)/, '\1,') if modulo(1) > 0
+      self.to_i.to_s
+    end
   end
 end
