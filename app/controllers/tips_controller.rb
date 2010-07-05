9a899e43b8c2251e595964aa8dedbc1bcd82988e
Len Smith <ignu.smith@gmail.com>
2010-07-05 18:14:02 -0400

    trying to fix heroku"

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -2,11 +2,11 @@ class TipsController < ApplicationController
 
   def post_from_twitter
      @show_sidebar = !request.xhr? 
-     render :layout => !request.xhr? 
+     render :layout => false
   end
 
   def format_your_update
-     response.headers['Cache-Control'] = 'no-cache'
+     response.headers['Cache-Control'] = 'public, max-age=500'
      @show_sidebar = !request.xhr? 
      render :layout => !request.xhr? 
   end
