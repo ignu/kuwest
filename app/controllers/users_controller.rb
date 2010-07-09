21fd11fe09d521ff297e4a1e75df53308069bb52
Len Smith <ignu.smith@gmail.com>
2010-07-09 02:31:47 -0400

    added top users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate_user!, :except => [:new, :show, :create]
+  before_filter :authenticate_user!, :except => [:new, :show, :create, :top]
   def add_stored_message 
       return unless cookies[:status]
       win_view_model = WinViewModel.new(
