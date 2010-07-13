d50ee8727d6b2eae276d579b92f283f8cadb4931
Sean Liu <seanliugm@gmail.com>
2010-07-13 21:40:41 +0800

    * fixed test failures in users_controller_spec and quest.feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   
   def show
     
-    @user = User.find(params[:id])
+    @user = User.find_by_id(params[:id])
     @page = params[:page]
     @page ||= 1
     if @user.nil?
